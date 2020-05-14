e0cc01fd57dc85bebd1517865562bad001ea5767
Nathan Redmond <53835861+NRedmond1@users.noreply.github.com>
2020-05-14 13:05:27 -0400

    Stock App

--- Project_2/app/src/main/java/com/example/project2/MainAct.java
+++ Project_2/app/src/main/java/com/example/project2/MainAct.java
@@ -9,34 +9,79 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Chronometer;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.text.*;
 import java.util.*;
-import java.time.format.DateTimeFormatter;
-import java.time.LocalDateTime;
-
-import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 
 
 public class MainAct extends AppCompatActivity {
 
         private ArrayList<Model> productList;
+        private ArrayList<String> symbol;
         int count = 0;
         Chronometer simple;
+        EditText eText;
         long elapsedTime;
+        int downCount;
         Thread thread;
-        String[] symbols = {"AAPL", "MSFT", "AMZN", "FB", "GOOGL", "IBM", "ACM", "ACN", "ADI", "SNAP"};
-        String[] symbolName = {"Apple", "Microsoft", "Amazon", "Facebook", "Google", "IBM", "ACM", "ACN", "ADI", "Snapchat"};
-        Model[] item = new Model[10];
-        AlphaVantage[] stock = new AlphaVantage[10];
+        String[] symbols;
+        String[] symbolName;
+        Model[] item;
+        AlphaVantage[] stock;
         listviewAdapter adapter;
-
         String message = "none";
+        int remainder;
+        int subremainder;
+
+    private void ReadTextFile(MainAct view) throws IOException {
+        //String string = "";
+        //StringBuilder stringBuilder = new StringBuilder();
+        int count = 0;
+        InputStream is = this.getResources().openRawResource(R.raw.nasdaq);
+        InputStream is2 = this.getResources().openRawResource(R.raw.nyse);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        BufferedReader reader2 = new BufferedReader(new InputStreamReader(is2));
+        String line = reader.readLine();
+        String[] temp = line.split("\t");
+        while (line != null) {
+            //Log.d(">>>", temp[1]);
+            temp = line.split("\t");
+            for(int i=0; i<symbol.size(); i++){
+                if(temp[0].equals(symbol.get(i))){
+                    symbols[count] = symbol.get(i);
+                    symbolName[count] = temp[1];
+                    count++;
+                }
+            }
+            line = reader.readLine();
+        }
+        String line2 = reader2.readLine();
+        String[] temp2 = line2.split("\t");
+        while (line2 != null) {
+            //Log.d(">>>", temp2[0]);
+            temp2 = line2.split("\t");
+            for(int i=0; i<symbol.size(); i++){
+                if(temp2[0].equals(symbol.get(i))){
+                    symbols[count] = symbol.get(i);
+                    symbolName[count] = temp2[1];
+                    count++;
+                }
+            }
+            line2 = reader2.readLine();
+        }
+        is.close();
+        is2.close();
+    }
 
     public void strategyradioClicked(View view) {
         // Is the button now checked?
@@ -69,15 +114,32 @@ public class MainAct extends AppCompatActivity {
         }
 
     }
-
+        private void populate(){
+            populateList();
+            adapter.notifyDataSetChanged();
+            simple.setBase(SystemClock.elapsedRealtime());
+            simple.start();
+        }
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_mainlist);
 
+            symbol=getIntent().getExtras().getStringArrayList("keyStocks");
 
-            for(int i=0; i<10; i++)
+            symbols = new String[symbol.size()];
+            symbolName = new String[symbol.size()];
+            item = new Model[symbol.size()];
+            stock = new AlphaVantage[symbol.size()];
+
+            try {
+                ReadTextFile(this);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            for(int i=0; i<symbol.size(); i++)
                 stock[i] = new AlphaVantage();
 
             productList = new ArrayList<Model>();
@@ -86,11 +148,15 @@ public class MainAct extends AppCompatActivity {
             lview.setAdapter(adapter);
 
             simple = new Chronometer(this);
+            eText = (EditText) findViewById(R.id.countdown);
 
             simple.setBase(SystemClock.elapsedRealtime());
             simple.start();
             elapsedTime = (SystemClock.elapsedRealtime() - simple.getBase())/1000000;
             populateList();
+
+            remainder = symbol.size()%5;
+            subremainder = 5-remainder;
 
             thread = new Thread(){
                 @Override
@@ -102,15 +168,38 @@ public class MainAct extends AppCompatActivity {
                                 @Override
                                 public void run() {
                                     Log.d("Time", "Elapsed" + elapsedTime);
+                                    downCount = 65 - ((int)(elapsedTime));
+                                    eText.setText(String.valueOf(downCount));
                                     if( elapsedTime > 65 ){
-                                        if(count == 0)
-                                            count = 5;
-                                        else
-                                            count = 0;
-                                        populateList();
-                                        adapter.notifyDataSetChanged();
-                                        simple.setBase(SystemClock.elapsedRealtime());
-                                        simple.start();
+                                        if( symbol.size()%5 == 0){
+                                            if(count+5 == symbol.size()){
+                                                count = 0;
+                                                populate();
+                                            }
+                                            else {
+                                                count = count +5;
+                                                populate();
+                                            }
+                                        }
+                                        else {
+                                            if (count+5 < (symbol.size() - remainder)) {
+                                                count = count + 5;
+                                                populate();
+                                            }
+                                            else if(count%5 != 0) {
+                                                count = 0;
+                                                populate();
+                                            }
+                                            else{
+                                                if(symbol.size() < 5){
+                                                    populate();
+                                                }
+                                                else {
+                                                    count = count + remainder;
+                                                    populate();
+                                                }
+                                            }
+                                        }
                                     }
                                     elapsedTime = (SystemClock.elapsedRealtime() - simple.getBase())/1000;
                                 }
@@ -123,7 +212,6 @@ public class MainAct extends AppCompatActivity {
                 }
             };
             thread.start();
-
 
             adapter.notifyDataSetChanged();
 
@@ -299,36 +387,101 @@ public class MainAct extends AppCompatActivity {
         boolean firstTime = true;
         @SuppressLint("DefaultLocale")
         private void populateList() {
-            try {
-                for (int x = count; x < count + 5; x++) {
-                    stock[x].executeFunc1(symbols[x]);
-                    stock[x].executeFunc2(symbols[x]);
+            if(count%5 == 0 && symbol.size() >= 5) {
+                try {
+                    for (int x = count; x < count + 5; x++) {
+                        stock[x].executeFunc1(symbols[x]);
+                        stock[x].executeFunc2(symbols[x]);
+                    }
+                } catch (Exception e) {
+                    //do nothing
                 }
-            } catch (Exception e) {
-                //do nothing
-            }
 
-            Float temp, temp2, temp3, temp4;
-            String t, t2, t3, t4;
-            for (int y = count; y < count + 5; y++) {
-                temp = Float.valueOf(stock[y].data[4]);
-                temp2 = Float.valueOf(stock[y].data[5]);
-                temp3 = Float.valueOf(stock[y].data[6]);
-                temp4 = Float.valueOf(stock[y].data[7]);
-                t = String.format("%.2f", temp);
-                t2 = String.format("%.2f", temp2);
-                t3 = String.format("%.2f", temp3);
-                t4 = String.format("%.2f", temp4);
+                Float temp, temp2, temp3, temp4;
+                String t, t2, t3, t4;
+                int sizehold;
+                for (int y = count; y < count + 5; y++) {
+                    temp = Float.valueOf(stock[y].data[4]);
+                    temp2 = Float.valueOf(stock[y].data[5]);
+                    temp3 = Float.valueOf(stock[y].data[6]);
+                    temp4 = Float.valueOf(stock[y].data[7]);
+                    t = String.format("%.2f", temp);
+                    t2 = String.format("%.2f", temp2);
+                    t3 = String.format("%.2f", temp3);
+                    t4 = String.format("%.2f", temp4);
 
-                item[y] = new Model(stock[y].data[0], symbolName[y], stock[y].data[1], stock[y].data[2], stock[y].data[3], t+" / "+t2, t3 + " / " + t4, stock[y].data[8]);
-                productList.add(y, item[y]);
-                if( firstTime == false )
-                    productList.remove(y+1);
-                //productList.add(item[y]);
+                    item[y] = new Model(stock[y].data[0], symbolName[y], stock[y].data[1], stock[y].data[2], stock[y].data[3], t + " / " + t2, t3 + " / " + t4, stock[y].data[8]);
+                    //sizehold = productList.size();
+                    productList.add(y, item[y]);
+                    if (firstTime == false)
+                        productList.remove(y + 1);
+                    //productList.add(item[y]);
+                }
+                if (count+5 == symbol.size())
+                    firstTime = false;
             }
-            if(count == 5)
+            else if(symbol.size() < 5){
+                try {
+                    for (int x = count; x < symbol.size(); x++) {
+                        stock[x].executeFunc1(symbols[x]);
+                        stock[x].executeFunc2(symbols[x]);
+                    }
+                } catch (Exception e) {
+                    //do nothing
+                }
+
+                Float temp, temp2, temp3, temp4;
+                String t, t2, t3, t4;
+                for (int y = count; y < symbol.size(); y++) {
+                    temp = Float.valueOf(stock[y].data[4]);
+                    temp2 = Float.valueOf(stock[y].data[5]);
+                    temp3 = Float.valueOf(stock[y].data[6]);
+                    temp4 = Float.valueOf(stock[y].data[7]);
+                    t = String.format("%.2f", temp);
+                    t2 = String.format("%.2f", temp2);
+                    t3 = String.format("%.2f", temp3);
+                    t4 = String.format("%.2f", temp4);
+
+                    item[y] = new Model(stock[y].data[0], symbolName[y], stock[y].data[1], stock[y].data[2], stock[y].data[3], t + " / " + t2, t3 + " / " + t4, stock[y].data[8]);
+                    productList.add(y, item[y]);
+                    if (firstTime == false)
+                        productList.remove(y + 1);
+                }
                 firstTime = false;
+            }
+            else{
+                int some = count+subremainder;
+                try {
+                    for (int x = some; x < some + remainder; x++) {
+                        stock[x].executeFunc1(symbols[x]);
+                        stock[x].executeFunc2(symbols[x]);
+                    }
+                } catch (Exception e) {
+                    //do nothing
+                }
 
+                Float temp, temp2, temp3, temp4;
+                String t, t2, t3, t4;
+                int sizehold;
+                for (int y = some; y < some + remainder; y++) {
+                    temp = Float.valueOf(stock[y].data[4]);
+                    temp2 = Float.valueOf(stock[y].data[5]);
+                    temp3 = Float.valueOf(stock[y].data[6]);
+                    temp4 = Float.valueOf(stock[y].data[7]);
+                    t = String.format("%.2f", temp);
+                    t2 = String.format("%.2f", temp2);
+                    t3 = String.format("%.2f", temp3);
+                    t4 = String.format("%.2f", temp4);
+
+                    item[y] = new Model(stock[y].data[0], symbolName[y], stock[y].data[1], stock[y].data[2], stock[y].data[3], t + " / " + t2, t3 + " / " + t4, stock[y].data[8]);
+                    //sizehold = productList.size();
+                    productList.add(y, item[y]);
+                    if (firstTime == false)
+                        productList.remove(y + 1);
+                    //productList.add(item[y]);
+                }
+                firstTime = false;
+            }
         }
 
 
--- Project_2/app/src/main/java/com/example/project2/MainActivity.java
+++ Project_2/app/src/main/java/com/example/project2/MainActivity.java
@@ -3,19 +3,53 @@ package com.example.project2;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.text.Editable;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    ArrayList<String> list;
+    Button btnAdd;
+    EditText editText;
+    ArrayAdapter<String> arrayAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.list2);
+        btnAdd = (Button) findViewById(R.id.btn_add);
+        editText = (EditText) findViewById(R.id.et_stock);
+        list = new ArrayList<String>();
+        arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, list);
+
+        btnAdd.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String stocks = editText.getText().toString();
+                list.add(stocks);
+                arrayAdapter.notifyDataSetChanged();
+                editText.getText().clear();
+            }
+        });
+
+        listView.setAdapter(arrayAdapter);
     }
 
       //TEST FUNCTION
     public void goToTest(View view){
         Intent intent = new Intent(this, MainAct.class);
+        intent.putStringArrayListExtra("keyStocks", list);
         startActivity(intent);
     }
 
